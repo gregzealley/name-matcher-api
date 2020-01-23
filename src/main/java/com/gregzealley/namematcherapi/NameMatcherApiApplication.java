c104aca6dca0a791df27cbcca36a023230d2c9f4
Greg Zealley <greg.zealley@localhost.localdomain>
2020-01-23 12:53:34 +0000

    Initial project creation

---
+++ src/main/java/com/gregzealley/namematcherapi/NameMatcherApiApplication.java
@@ -0,0 +1,13 @@
+package com.gregzealley.namematcherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameMatcherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NameMatcherApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gregzealley/namematcherapi/NameMatcherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gregzealley.namematcherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameMatcherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
