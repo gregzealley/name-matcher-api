a083d1eeec821a803391094c4302575c98ba031e
Greg Zealley <greg@zealley.com>
2020-01-31 12:06:29 +0000

    Ensure initial test is passing

--- src/test/java/com/gregzealley/namematcherapi/NameMatcherApiIT.java
+++ src/test/java/com/gregzealley/namematcherapi/NameMatcherApiIT.java
@@ -1,21 +1,31 @@
 package com.gregzealley.namematcherapi;
 
+import io.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Value;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.when;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class NameMatcherApiIT {
 
-    @Value("${local.server.port}")
-    private int serverPort;
+    @LocalServerPort
+    private int port;
+
+    @Before
+    public void setup() {
+        RestAssured.port = port;
+    }
 
     @Test
     public void whenPassTwoCorrectParametersThenCorrectStatusCodeIsReturned() {
         when()
-                .get("/nameMatch&primaryList=plist&secondaryList=sList")
+                .get("nameMatch?primaryList=aaa&secondaryList=ccc")
                 .then()
                 .statusCode(200);
     }
