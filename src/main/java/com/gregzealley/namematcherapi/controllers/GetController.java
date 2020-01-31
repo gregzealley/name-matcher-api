d6a2a10459547d017293f15be7f178d54d1285d1
Greg Zealley <greg@zealley.com>
2020-01-31 16:18:45 +0000

    Initial changes to endpoint

--- src/main/java/com/gregzealley/namematcherapi/controllers/GetController.java
+++ src/main/java/com/gregzealley/namematcherapi/controllers/GetController.java
@@ -1,16 +1,41 @@
 package com.gregzealley.namematcherapi.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.gregzealley.namematcherapi.services.NameMatchService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class GetController {
 
-    @GetMapping(value = "/nameMatch")
-    public String nameMatch(@RequestParam(value = "primaryList") String primaryList,
-                            @RequestParam(value = "secondaryList") String secondaryList)
-    {
-        return "Primary List: " + primaryList + " Secondary List: " + secondaryList;
+    private final NameMatchService nameMatchService;
+
+    public GetController(final NameMatchService nameMatchService) {
+        this.nameMatchService = nameMatchService;
+    }
+
+//    @GetMapping(value = "/nameMatch")
+//    public String nameMatch(@RequestParam(value = "primaryList") String primaryList,
+//                            @RequestParam(value = "secondaryList") String secondaryList) {
+//        return "Primary List: " + primaryList + " Secondary List: " + secondaryList;
+//    }
+
+    @RequestMapping(value = "/singleFile", consumes = "text/plain", method = RequestMethod.POST)
+    public String uploadSingleFile(@RequestParam("file") MultipartFile file) throws IOException {
+
+        String completeData = "";
+
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                completeData = new String(bytes);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return completeData;
     }
 }
+
+
---
+++ src/main/java/com/gregzealley/namematcherapi/services/NameMatchService.java
@@ -0,0 +1,7 @@
+package com.gregzealley.namematcherapi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NameMatchService {
+}
