f74dab25a3ef233134849ab95e9f9ab203e87bf1
Greg Zealley <greg.zealley@localhost.localdomain>
2020-02-04 16:55:07 +0000

    Initial commit - work in progress

---
+++ src/test/java/com/gregzealley/namematcherapi/ClassTest.java
@@ -0,0 +1,12 @@
+package com.gregzealley.namematcherapi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ClassTest {
+
+    @Test
+    public void simpleTest() {
+        Assert.assertFalse(false);
+    }
+}
