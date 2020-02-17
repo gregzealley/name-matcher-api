3924ea96b9ddcb59dabc5a709caaf671f201a762
Greg Zealley <greg.zealley@ukho.gov.uk>
2020-02-17 16:20:49 +0000

    Add working endpoint for single file

--- src/main/java/com/gregzealley/namematcherapi/controllers/GetController.java
+++ src/main/java/com/gregzealley/namematcherapi/controllers/GetController.java
@@ -2,11 +2,14 @@ package com.gregzealley.namematcherapi.controllers;
 
 import com.gregzealley.namematcherapi.services.NameMatchService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @RestController
 public class GetController {
@@ -17,38 +20,21 @@ public class GetController {
         this.nameMatchService = nameMatchService;
     }
 
-//    @GetMapping(value = "/nameMatch")
-//    public String nameMatch(@RequestParam(value = "primaryList") String primaryList,
-//                            @RequestParam(value = "secondaryList") String secondaryList) {
-//        return "Primary List: " + primaryList + " Secondary List: " + secondaryList;
-//    }
+    @RequestMapping(value = "/singleFile", method = RequestMethod.POST)
+    public String uploadSingleFile(@RequestParam(value = "file") MultipartFile file) {
 
-    @RequestMapping(value = "/singleFile", headers = "content-type=multipart/*")
-    //@RequestMapping(value = "/singleFile", method = RequestMethod.POST, consumes = {"multipart/form-data"}, produces = {"text/plain"})
-    public String uploadSingleFile(@RequestParam(value = "file") MultipartFile file) throws IOException {
+        String completeData = "";
 
-//        String completeData = "";
-//
-//        if (!file.isEmpty()) {
-//            try {
-//                byte[] bytes = file.getBytes();
-//                completeData = new String(bytes);
-//            } catch (IOException e) {
-//                System.err.println(e.getMessage());
-//            }
-//        }
-//        return completeData;
-        return "It works!";
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                completeData = new String(bytes, UTF_8);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        return completeData;
     }
-
-
-//    // This works but only grabs the filepath, not the content... plan b
-//    @RequestMapping(value = "/singleFile", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-//    public String getAcquisition(@RequestParam(value = "file", required = true) String filePath) throws FileNotFoundException {
-//
-//        return filePath;
-//
-//    }
 }
 
 
