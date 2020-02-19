059b20c5c50bcdb712f10ca6ac937c0dc4879a06
Greg Zealley <greg@zealley.com>
2020-02-19 20:23:56 +0000

    Add API test - happy path

--- src/integration-test/java/apitests/NameMatcherApiIT.java
+++ src/integration-test/java/apitests/NameMatcherApiIT.java
@@ -2,6 +2,8 @@ package apitests;
 
 import com.gregzealley.namematcherapi.NameMatcherApiApplication;
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -24,9 +29,24 @@ public class NameMatcherApiIT {
     }
 
     @Test
-    public void whenPassTwoCorrectParametersThenCorrectStatusCodeIsReturned() {
-        assertThat(true)
-                .isTrue();
+    public void whenPassTwoPopulatedFilesThenGetCorrectResponse() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file1 = new File(classLoader.getResource("api_test_files/populated_file_1.txt").getFile());
+        File file2 = new File(classLoader.getResource("api_test_files/populated_file_2.txt").getFile());
+
+        ExtractableResponse response = given()
+                .multiPart("primary_file", file1)
+                .multiPart("secondary_file", file2)
+                .when()
+                .post("uploadfiles")
+                .then()
+                .extract();
+
+        assertThat(response.statusCode())
+                .isEqualTo(HttpStatus.SC_OK);
+
+        assertThat(response.body().asString())
+                .isEqualTo("There are 4 rows in the primary file and 3 in the secondary file.");
     }
 
 }
