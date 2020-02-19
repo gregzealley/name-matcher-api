80cbb42770af5071a55eae895a76465027cd86a9
Greg Zealley <greg.zealley@ukho.gov.uk>
2020-02-19 16:57:16 +0000

    Refactor internal working of service

--- src/main/java/com/gregzealley/namematcherapi/services/NameMatcherService.java
+++ src/main/java/com/gregzealley/namematcherapi/services/NameMatcherService.java
@@ -1,5 +1,6 @@
 package com.gregzealley.namematcherapi.services;
 
+import com.gregzealley.namematcherapi.enums.FileType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,29 +9,33 @@ import java.io.IOException;
 @Service
 public class NameMatcherService {
 
+    private final String NEW_LINE_REGEX = "[\\r\\n]+";
+
+    private String[] primaryFileContent;
+    private String[] secondaryFileContent;
+
     public String initiateMatch(final MultipartFile primaryFile, final MultipartFile secondaryFile) {
 
-        String[] primaryFileContent = new String[0];
-        String[] secondaryFileContent = new String[0];
-
-        primaryFileContent = convertToArray(primaryFile, primaryFileContent);
-        secondaryFileContent = convertToArray(secondaryFile, secondaryFileContent);
+        importFileIntoArray(primaryFile, FileType.PRIMARY );
+        importFileIntoArray(secondaryFile, FileType.SECONDARY);
 
         return String.format("There are %s rows in the primary file and %s in the secondary file.",
                 primaryFileContent.length, secondaryFileContent.length);
     }
 
-    private String[] convertToArray(MultipartFile primaryFile, String[] primaryFileContent) {
-        if (!primaryFile.isEmpty()) {
+    private void importFileIntoArray(MultipartFile file, FileType type) {
+        if (!file.isEmpty()) {
             try {
-                byte[] bytes = primaryFile.getBytes();
+                byte[] bytes = file.getBytes();
                 String completeData = new String(bytes);
 
-                primaryFileContent = completeData.split("[\\r\\n]+");
+                if (type == FileType.PRIMARY)
+                primaryFileContent = completeData.split(NEW_LINE_REGEX);
+                else
+                    secondaryFileContent = completeData.split(NEW_LINE_REGEX);
             } catch (IOException e) {
                 System.err.println(e.getMessage());
             }
         }
-        return primaryFileContent;
     }
 }
