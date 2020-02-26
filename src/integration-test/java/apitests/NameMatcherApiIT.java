12f4d02fb75064a5df0cc281c8951e9eb106e5ef
gregzealley <47225721+gregzealley@users.noreply.github.com>
2020-02-26 20:22:29 +0000

    Add happy path API tests
     - Happy path only
     - Not able to pass empty params to API

--- src/integration-test/java/apitests/NameMatcherApiIT.java
+++ src/integration-test/java/apitests/NameMatcherApiIT.java
@@ -1,7 +1,11 @@
 package apitests;
 
+import apitests.services.NameMatcherApiTestService;
 import com.gregzealley.namematcherapi.NameMatcherApiApplication;
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,11 +13,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+import java.util.Objects;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = NameMatcherApiApplication.class)
 public class NameMatcherApiIT {
+
+    private NameMatcherApiTestService nameMatcherApiTestService;
 
     @LocalServerPort
     private int port;
@@ -21,12 +30,70 @@ public class NameMatcherApiIT {
     @Before
     public void setup() {
         RestAssured.port = port;
+        nameMatcherApiTestService = new NameMatcherApiTestService();
     }
 
     @Test
-    public void whenPassTwoCorrectParametersThenCorrectStatusCodeIsReturned() {
-        assertThat(true)
-                .isTrue();
+    public void whenPostTwoPopulatedTxtFilesThenGetOkStatusAndCorrectMessage() {
+
+        File file1 = readFileFromTestResources("api_test_files/txt_file_populated_1.txt");
+        File file2 = readFileFromTestResources("api_test_files/txt_file_populated_2.txt");
+
+        ExtractableResponse<Response> response = nameMatcherApiTestService.callUploadFiles(file1, file2);
+
+        assertThat(response.statusCode())
+                .isEqualTo(HttpStatus.SC_OK);
+
+        assertThat(response.body().asString())
+                .isEqualTo("There are 4 rows in the primary file and 3 in the secondary file.");
     }
 
+    @Test
+    public void whenPostTwoEmptyTxtFilesThenGetOkStatusAndCorrectMessage() {
+
+        File file1 = readFileFromTestResources("api_test_files/txt_file_empty_1.txt");
+        File file2 = readFileFromTestResources("api_test_files/txt_file_empty_2.txt");
+
+        ExtractableResponse<Response> response = nameMatcherApiTestService.callUploadFiles(file1, file2);
+
+        assertThat(response.statusCode())
+                .isEqualTo(HttpStatus.SC_OK);
+
+        assertThat(response.body().asString())
+                .isEqualTo("There are 0 rows in the primary file and 0 in the secondary file.");
+    }
+
+    @Test
+    public void whenPostTwoCsvPopulatedFilesThenGetOkStatusAndMessage() {
+
+        File file1 = readFileFromTestResources("api_test_files/csv_file_populated_1.csv");
+        File file2 = readFileFromTestResources("api_test_files/csv_file_populated_2.csv");
+
+        ExtractableResponse<Response> response = nameMatcherApiTestService.callUploadFiles(file1, file2);
+
+        assertThat(response.statusCode())
+                .isEqualTo(HttpStatus.SC_OK);
+
+        assertThat(response.body().asString())
+                .isEqualTo("There are 4 rows in the primary file and 3 in the secondary file.");
+
+    }
+
+//    @Test
+//    public void whenPostNoFilesThenGetMalformedRequestStatus() {}
+//
+//    @Test
+//    public void whenPostOnlyPrimaryFileThenGetMalformedRequestStatus() {}
+//
+//    @Test
+//    public void whenPostOnlySecondaryFileThenGetMalformedRequestStatus() {}
+//
+//    @Test
+//    public void whenPostOneNonTextFileThenGetGetMalformedRequestStatus() {}
+
+    private File readFileFromTestResources(final String filename) {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new File(Objects.requireNonNull(classLoader.getResource(filename)).getFile());
+    }
 }
---
+++ src/integration-test/java/apitests/services/NameMatcherApiTestService.java
@@ -0,0 +1,22 @@
+package apitests.services;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
+public class NameMatcherApiTestService {
+
+    public ExtractableResponse<Response> callUploadFiles(final File primaryFile, final File secondaryFile) {
+
+        return given()
+                .multiPart("primary_file", primaryFile)
+                .multiPart("secondary_file", secondaryFile)
+                .when()
+                .post("uploadfiles")
+                .then()
+                .extract();
+    }
+}
